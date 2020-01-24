21c34d3886fa3bb17c8a80336a5f0d6efd43c0dc
Dave Brett <david.brett@tessella.com>
2020-01-24 14:22:05 +0000

    script updates

---
+++ infrastructure/images/push.sh
@@ -0,0 +1,2 @@
+docker build -t davidbrett88/envsubst:0.1.0 -f Dockerfile.envsubst .
+docker push davidbrett88/envsubst:0.1.0 
--- infrastructure/pipelines/meta/generatePipeline.sh
+++ infrastructure/pipelines/meta/generatePipeline.sh
@@ -1,3 +1,4 @@
 set -e -u -x
 
+echo "" > generated-pipelines.yaml
 ls meta-pipeline/services | awk '{ print "SERVICE="$1" envsubst < meta-pipeline/infrastructure/pipelines/meta/gosvcpipeline.yaml >> generated-pipelines.yaml" }' | sh
