4f60b8483c5d9540855837eb587a28b118668bd9
Dave Brett <david.brett@tessella.com>
2020-01-24 15:17:51 +0000

    fix script

--- infrastructure/pipelines/meta/generatePipeline.sh
+++ infrastructure/pipelines/meta/generatePipeline.sh
@@ -1,7 +1,7 @@
 set -e -u -x
 
 ls meta-pipeline/services | awk '{ print "SERVICE="$1" envsubst < meta-pipeline/infrastructure/pipelines/meta/gosvcpipeline.yaml > out/"$1".yaml" }' | sh
-echo $'pipelines:' > out/generated-pipelines.yaml
+echo 'pipelines:' > out/generated-pipelines.yaml
 ls meta-pipeline/services | awk '{ print "SERVICE="$1" envsubst < meta-pipeline/infrastructure/pipelines/meta/metapipeline.yaml >> out/generated-pipelines.yaml" }' | sh
 
 cat out/generated-pipelines.yaml
